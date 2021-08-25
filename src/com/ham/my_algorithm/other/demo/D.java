4377d51ceda1cb92da1b03ebbdf357f0441fbdd8
ham <672724425@qq.com>
2021-08-25 23:29:02 +0800

    静态内部类特点

--- src/com/ham/my_algorithm/other/demo/D.java
+++ src/com/ham/my_algorithm/other/demo/D.java
@@ -1,4 +1,4 @@
 package com.ham.my_algorithm.other.demo;
 
-public interface D extends C {
+public interface D {
 }
---
+++ src/com/ham/my_algorithm/other/demo/OutClass1.java
@@ -0,0 +1,27 @@
+package com.ham.my_algorithm.other.demo;
+
+public class OutClass1 {
+    public int oid;
+    public String oname;
+    public static String oage;
+//    只有静态内部类才能够定义静态的成员变量与成员方法
+//    静态内部类只能引用外部类中的静态的成员（变量或方法），而不能够访问非静态的变量。
+    public static class InnerStaticClass1 {
+        public int iid;
+        public String iname;
+
+        public static void main(String[] args) {
+            oage = "f";
+        }
+    }
+
+    public class InnerClass2 {
+        public int iid;
+        public String iname;
+        public static String name;
+
+        public void mainw(String[] args) {
+            oname = "1";
+        }
+    }
+}
